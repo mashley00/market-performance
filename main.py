62473b3a5da5752be009f927da22e8a0d27bf8ca
Mike Ashley <mashley00@gmail.com>
2025-05-22 19:17:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from fastapi.staticfiles import StaticFiles
 from geo_decay import router as geo_decay_router
 from fb_insights import router as fb_insights_router
 from fb_targeting import router as fb_targeting_router
-# ❌ campaign_db is not a router, so don't import it as one
+from campaign_db import router as campaign_db_router  # TEMP: to init DB
 
 app = FastAPI()
 
-# Attach working routers
+# Attach routers
 app.include_router(geo_decay_router)
 app.include_router(fb_insights_router)
 app.include_router(fb_targeting_router)
+app.include_router(campaign_db_router)  # TEMP: needed to hit /init-db
 
 # Serve static files (if needed)
 app.mount("/static", StaticFiles(directory="static"), name="static")
